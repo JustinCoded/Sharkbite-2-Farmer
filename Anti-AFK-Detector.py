69e48456b612f05b2f1fbb5460e38b7ae41eb9ec
Justin Doehling <justindoehling@Justins-MBP.lan>
2022-12-20 06:12:32 -0600

    MacBook Support and new game loop

--- Anti-AFK-Detector.py
+++ Anti-AFK-Detector.py
@@ -1,195 +1,181 @@
 from time import sleep
 import keyboard
 from random import randint
-import base64
 import pyautogui
 import time
-data = "Q3JlYXRlZCBCeTogQ3JhY2tvMjk4" ; data1 = base64.b64decode(data) ; data0 = data1.decode('utf-8') ; keypress = 'alt'
-dat = "Q2xpY2sgdGhlIFJvYmxveCBHYW1lIFdpbmRvdyAmIFByZXNzIHRoZSAnY3RybCcgS2V5IQ==" ; dat1 = base64.b64decode(dat) ; dat0 = dat1.decode('utf-8')
-da = "VG8gRXhpdC4gUHJlc3MgdGhlIGFuZCBob2xkICdhbHQnIEtleS4gKEFmdGVyIHlvdSBoZXJlIGEgbm90aWZpY2F0aW9uIHNvdW5kIFByZXNzICdhbHQnIHR3aWNlKS4=" ; da1 = base64.b64decode(da) ; da0 = da1.decode('utf-8')
+import platform
 
+print("Create by: Me\n")
+print("Click the Roblox Game Window & Press the 'F1' Key!\n")
+print("Ctrl-C to exit")
 
-print(data0) ; print(" ") ; print(dat0) ; print(da0) ; print(" ")
 localtime = time.asctime(time.localtime(time.time()))
 
-keyboard.wait('Ctrl')
+print(platform.system)
 
-while keyboard.read_key() != "alt":
-    subconf = pyautogui.locateCenterOnScreen('subconfirm.png', confidence=0.55)
-    under0 = pyautogui.locateCenterOnScreen('underwater.png', confidence=0.55)
-    under1 = pyautogui.locateCenterOnScreen('under.png', confidence=0.55)
-    under2 = pyautogui.locateCenterOnScreen('darkestwat.png', confidence=0.55)
-    under3 = pyautogui.locateCenterOnScreen('darkhill.png', confidence=0.55)
-    under4 = pyautogui.locateCenterOnScreen('darkwat.png', confidence=0.55)
-    under5 = pyautogui.locateCenterOnScreen('lightestwat.png', confidence=0.55)
-    under6 = pyautogui.locateCenterOnScreen('lightwat.png', confidence=0.55)
-    keyboard.press_and_release('Ctrl')
+keyboard.wait('f1')
 
+def detectMode():
+    # detect lobby or game mode 
+    if (isAFKDisplayed()):
+        print("AFK Handled")     
+    elif (isInGame()):
+        if (isLobby()):
+            print("Detected Lobby Mode") 
+        else:
+            if (isShark()):
+    #            sharkMovement()
+                print("Detected Shark Mode")
+            elif (isDriving()):
+                print("Detected Driving Mode") 
+    #            hunterMovement()
+            elif (isBoatPlayerSelection()):
+                sleep(20)
 
-    def Sub_Movement():
-        keyboard.press_and_release('Ctrl')
-        global subconf
-        keyboard.press('q')
-        sleep(randint(3,6))
-        keyboard.release('q')
-        keyboard.press('e')
-        sleep(randint(3,6))
-        keyboard.release('e')
-
-    keyboard.press('a')
-    sleep(randint(2,8))
-    keyboard.release('a')
-    keyboard.press('d')
-    sleep(randint(2,7))
-    keyboard.release('d')
-
-    if subconf != None:
-        Sub_Movement()
-    if under0 != None:
-        Sub_Movement()
-    if under1 != None:
-        Sub_Movement()
-    if under2 != None:
-        Sub_Movement()
-    if under3 != None:
-        Sub_Movement()
-    if under4 != None:
-        Sub_Movement()
-    if under5 != None:
-        Sub_Movement()
-    if under6 != None:
-        Sub_Movement()
-
-    if subconf != True:
-        keyboard.press_and_release('Ctrl')
-        keyboard.press('a')
-        sleep(randint(2,8))
-        keyboard.release('a')
-        keyboard.press('d')
-        sleep(randint(2,7))
-        keyboard.release('d')
-
-    if under0 != True:
-        keyboard.press_and_release('Ctrl')
-        keyboard.press('a')
-        sleep(randint(2,8))
-        keyboard.release('a')
-        keyboard.press('d')
-        sleep(randint(2,7))
-        keyboard.release('d')
-
-    if under1 != True:
-        keyboard.press_and_release('Ctrl')
-        keyboard.press('a')
-        sleep(randint(2,8))
-        keyboard.release('a')
-        keyboard.press('d')
-        sleep(randint(2,7))
-        keyboard.release('d')
-
-    if under2 != True:
-        keyboard.press_and_release('Ctrl')
-        keyboard.press('a')
-        sleep(randint(2,8))
-        keyboard.release('a')
-        keyboard.press('d')
-        sleep(randint(2,7))
-        keyboard.release('d')
-
-    if under3 != True:
-        keyboard.press_and_release('Ctrl')
-        keyboard.press('a')
-        sleep(randint(2,8))
-        keyboard.release('a')
-        keyboard.press('d')
-        sleep(randint(2,7))
-        keyboard.release('d')
-
-    if under4 != True:
-        keyboard.press_and_release('Ctrl')
-        keyboard.press('a')
-        sleep(randint(2,8))
-        keyboard.release('a')
-        keyboard.press('d')
-        sleep(randint(2,7))
-        keyboard.release('d')
-
-    if under5 != True:
-        keyboard.press_and_release('Ctrl')
-        keyboard.press('a')
-        sleep(randint(2,8))
-        keyboard.release('a')
-        keyboard.press('d')
-        sleep(randint(2,7))
-        keyboard.release('d')
-
-    if under6 != True:
-        keyboard.press_and_release('Ctrl')
-        keyboard.press('a')
-        sleep(randint(2,8))
-        keyboard.release('a')
-        keyboard.press('d')
-        sleep(randint(2,7))
-        keyboard.release('d')
-
-
-    def shark_movement1():
-        shark_movement = randint(1,6)
-        if shark_movement == 1:
-            keyboard.press('a')
-            print("       Player Moved 'Left' As The Shark.")
-            sleep(randint(2,10))
-            keyboard.release('a')
-
-        if shark_movement == 2:
-            keyboard.press('w')
-            print("       Player Moved 'Forward' As The Shark.")
-            sleep(randint(2,10))
-            keyboard.release('w')
-
-        if shark_movement == 3:
-            keyboard.press('s')
-            print("       Player Moved 'Backward' As The Shark.")
-            sleep(randint(2,10))
-            keyboard.release('s')
-
-        if shark_movement == 4:
-            keyboard.press('d')
-            print("       Player Moved 'Right' As The Shark.")
-            sleep(randint(2,10))
-            keyboard.release('d')
-        
-        if shark_movement == 5:
-            print("       Player Used The 'Speed Special' As The Shark.")
-            sleep(randint(1,4))
-            keyboard.press_and_release('Space')
-
-
-
-    shark_ifso = pyautogui.locateCenterOnScreen('shark.png', confidence=0.55)
-    shark_special = pyautogui.locateOnScreen('go.png', confidence=0.55)
-    ducksubmarine = pyautogui.locateCenterOnScreen('ducksub.png', confidence=0.55)
-    militarymarine = pyautogui.locateCenterOnScreen('milsub.png', confidence=0.55)
-
-    if militarymarine != None:
-        print("Player Used 'Military Submarine' as Submarine. - "+ localtime)
-        sleep(randint(10,14))
-        Sub_Movement()
+    elif (isGameEntry()):
+        print("Detected Game Entry")
+    elif (isPrizeWindow()):
+        print("Detected Prize Windw")
     
-    if ducksubmarine != None:
-        print("Player Used 'Duckmarine' as Submarine. - "+ localtime)
-        sleep(randint(10,14))
-        Sub_Movement()
+    else: 
+        print("Unknown Mode") 
+        # pyautogui.screenshot(str(time.time()) + '_unknown_mode.png')
+    
+def mainLoop():
+    detectMode()
 
-    def Shark_1Special():
-        shark_movement1()
-        
-    if shark_ifso != None:
-        print("Player Was Selected To Be Shark. - "+ localtime)
-        sleep(randint(20,25))
-        Shark_1Special()
+def sharkMovement():
+    shark_movement = randint(1,6)
+    if shark_movement == 1:
+        keyboard.press('a')
+        print("       Player moved 'Left' as the shark.")
+        sleep(randint(2,10))
+        keyboard.release('a')
 
-    if shark_special != None:
-        Shark_1Special()
+    if shark_movement == 2:
+        keyboard.press('w')
+        print("       Player moved 'Forward' as the shark.")
+        sleep(randint(2,10))
+        keyboard.release('w')
+
+    if shark_movement == 3:
+        keyboard.press('s')
+        print("       Player moved 'Backward' as the shark.")
+        sleep(randint(2,10))
+        keyboard.release('s')
+
+    if shark_movement == 4:
+        keyboard.press('d')
+        print("       Player moved 'Right' as the shark.")
+        sleep(randint(2,10))
+        keyboard.release('d')
+    
+    if shark_movement == 5:
+        print("       Player Used The 'Speed Special' as the shark.")
+        sleep(randint(1,4))
+        keyboard.press_and_release('Space')
+
+def hunterMovement():
+    hunter_movement = randint(1,6)
+    if hunter_movement == 1:
+        keyboard.press('a')
+        print("       Player moved 'Left' as the hunter.")
+        sleep(randint(2,10))
+        keyboard.release('a')
+
+    if hunter_movement == 2:
+        keyboard.press('w')
+        print("       Player moved 'Forward' as the hunter.")
+        sleep(randint(2,10))
+        keyboard.release('w')
+
+    if hunter_movement == 3:
+        keyboard.press('s')
+        print("       Player moved 'Backward' as the hunter.")
+        sleep(randint(2,10))
+        keyboard.release('s')
+
+    if hunter_movement == 4:
+        keyboard.press('d')
+        print("       Player moved 'Right' as the hunter.")
+        sleep(randint(2,10))
+        keyboard.release('d')
+
+def isShark() -> bool:
+    # Detect Shark by Special
+    goButton = pyautogui.locateOnScreen('samples/shark_accel.png', confidence=0.55)
+    if (goButton != None):
+        return True
+    else:
+        return False 
+
+def isDriving() -> bool:
+    # Detect Shark by Special
+    speedo = pyautogui.locateOnScreen('samples/speedo.png', confidence=0.25)
+    if (speedo != None):
+        return True
+    else:
+        return False 
+
+def isAFKDisplayed() -> bool:
+    afkBox = pyautogui.locateOnScreen('samples/afk_box.png', confidence=0.55)
+    if (afkBox != None):
+        afk_yesButtonCenter = pyautogui.center(afkBox)
+        pyautogui.click(x= (afk_yesButtonCenter.x /2)+20, y= (afk_yesButtonCenter.y /2)+20)
+    else:
+        return False 
+
+def isLobby() -> bool:
+    spectateButton = pyautogui.locateOnScreen('samples/spectate.png', confidence=0.55)
+    if (spectateButton != None):
+        screenSizeX, screenSizeY = pyautogui.size()
+        pyautogui.moveTo(screenSizeX / 2, screenSizeY / 2)
+        return True
+    else:
+        return False 
+
+def isGameEntry() -> bool: 
+    playButton = pyautogui.locateOnScreen('samples/play.png', confidence=0.55)
+    if (playButton != None):
+        playButtonCenter = pyautogui.center(playButton)
+        print("Center of Play at: " + str(playButtonCenter))
+        pyautogui.click(x=playButtonCenter.x / 2, y=playButtonCenter.y / 2)
+        return True
+    else:
+        return False 
+
+def isPrizeWindow() -> bool: 
+    prizeWindow = pyautogui.locateOnScreen('samples/prize.png', confidence=0.55)
+    if (prizeWindow != None):
+        closeButton = prizeWindow = pyautogui.locateOnScreen('samples/close_button.png', confidence=0.55)
+        closeButtonCenter = pyautogui.center(closeButton)
+        pyautogui.click(x=closeButtonCenter.x / 2, y=closeButtonCenter.y / 2)
+        return True
+    else:
+        return False 
+
+def isInGame() -> bool:
+    inGameIcons = pyautogui.locateOnScreen('samples/in_game.png', confidence=0.55)
+    if (inGameIcons != None):
+        return True
+    else:
+        return False 
+
+#def isSurvivor(): 
+
+def isBoatPlayerSelection(): 
+    otherPlayersText = pyautogui.locateOnScreen('samples/boat_player_selection.png', confidence=0.55)
+    if (otherPlayersText != None):
+        return True
+    else:
+        return False 
+
+print("Detecting Mode")
+while True: 
+    try:
+        mainLoop()
+        time.sleep(3)
+    except KeyboardInterrupt:
+        break
 
 exit()
